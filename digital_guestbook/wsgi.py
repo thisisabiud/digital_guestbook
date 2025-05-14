827d7ded8b1de0cb5dabc3066c891c384a1abbff
samo <iam.abiud@gmail.com>
2025-05-14 14:35:44 +0300

    fix wsgi

--- digital_guestbook/wsgi.py
+++ digital_guestbook/wsgi.py
@@ -1,9 +1,18 @@
+"""
+WSGI config for project_name project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/stable/howto/deployment/wsgi/
+"""
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-
-
+# Set the default Django settings module for the 'wsgi' application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_guestbook.settings')
 
+# This is the WSGI application object that servers will use
 application = get_wsgi_application()
