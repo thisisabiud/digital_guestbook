0a49c77b401eeb80d9a53b3db7519dbc0f9bae48
samo <iam.abiud@gmail.com>
2025-05-14 14:30:09 +0300

    wsgi

--- digital_guestbook/wsgi.py
+++ digital_guestbook/wsgi.py
@@ -1,15 +1,9 @@
 import os
-from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
-load_dotenv()
 
-environment = os.getenv('ENVIRONMENT', 'development')
 
-if environment == 'production':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_guestbook.settings.prod')
-else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_guestbook.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_guestbook.settings')
 
 application = get_wsgi_application()
